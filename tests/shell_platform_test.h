b92f245bfbc682b6cf8091e9be1db0f8685a42c8
RigelNostdlib <edo.nostdlib@gmail.com>
2026-03-19 20:37:51 +0400

    fix: add ShellProcessTests to enhance shell process testing coverage

---
+++ tests/shell_platform_test.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "lib/runtime.h"
+#include "shell_process.h"
+#include "tests.h"
+
+class ShellProcessTests
+{
+public:
+    static BOOL RunAll()
+    {
+        BOOL allPassed = true;
+
+        LOG_INFO("Running shell process Tests...");
+
+        RunTest(allPassed, &TestShellProcessCreate, "Shell Process Create");
+
+        if (allPassed)
+            LOG_INFO("All shell process tests passed!");
+        else
+            LOG_ERROR("Some shell process tests failed!");
+
+        return allPassed;
+    }
+
+private:
+    static BOOL TestShellProcessCreate()
+    {
+        auto createResult = ShellProcess::Create();
+        if (!createResult)
+        {
+            LOG_ERROR("Shell creation failed (error: %e)", createResult.Error());
+            return false;
+        }
+
+        LOG_INFO("  PASSED: Shell creation");
+        return true;
+    }
+};
